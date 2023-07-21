f3d6129565f002677fe3b6f322cecd4275147538
dungbeo26 <dungbeo26072002@gmail.com>
2023-07-21 16:55:23 +0700

    Create A6.php

---
+++ BTTH01/A6.php
@@ -0,0 +1,24 @@
+<?php
+
+$keys = array(
+    "field1" => "first",
+    "field2" => "second",
+    "field3" => "third"
+);
+
+$values = array(
+    "field1value" => "dinosaur",
+    "field2value" => "pig",
+    "field3value" => "platypus"
+);
+
+$keysAndValues = array();
+
+foreach ($keys as $field => $key) {
+    $valueKey = $field . 'value';
+    if (isset($values[$valueKey])) {
+        $keysAndValues[$key] = $values[$valueKey];
+    }
+}
+
+print_r($keysAndValues);
