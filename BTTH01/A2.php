9412405e1119663300731fb127cd7f8ae859e4e7
dungbeo26 <dungbeo26072002@gmail.com>
2023-07-21 16:50:42 +0700

    Create A2.php

---
+++ BTTH01/A2.php
@@ -0,0 +1,10 @@
+<?php
+
+$arrs = ['đỏ', 'xanh', 'cam', 'trắng'];
+
+$result = "Màu {$arrs[0]} là màu yêu thích của Anh, ";
+$result .= "Màu {$arrs[1]} là màu yêu thích của Sơn, ";
+$result .= "Màu {$arrs[2]} là màu yêu thích của Thắng, ";
+$result .= "còn màu yêu thích của tôi là màu {$arrs[3]}";
+
+echo $result;
